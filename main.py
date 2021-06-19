61dd125a6bfd1476c14b55403563362fbf8bc1ab
Ivan <hobbeat@mail.ru>
2021-06-19 17:05:03 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,57 @@
+import telebot
+from secret import TOKEN
+
+HELP = '''
+/help - напечатать список команд
+/add - добавить задачу в список
+/show - напечатать все добавленные задачи.
+'''
+
+bot = telebot.TeleBot(TOKEN)
+
+tasks = {}
+
+# def get_command(message):
+#     ...
+
+
+@bot.message_handler(commands=['start', 'help'])
+def start(message):
+    bot.reply_to(message, HELP)
+
+
+@bot.message_handler(commands=['add'])
+def add(message):
+    ...
+#     task = input("Введите задачу: ")
+#     date = input("Введите дату: ")
+#     if not date in tasks:
+#         tasks[date] = []
+#     tasks[date].append(task)
+#     print(f"Задача {task} добавлена на дату {date}")
+
+
+@bot.message_handler(commands=['show'])
+def show(message):
+    ...
+    # print(tasks)
+    # date = input("Введите дату: ")
+    # if date in tasks:
+    #     for task in tasks[date]:
+    #         print('[ ]', task)
+    # else:
+    #     print(f'Задач на {date} нет')
+
+
+@bot.message_handler(content_types=['text'])
+def echo_all(message):
+    text = ''
+    if '111' in message.text:
+        text = '999 ' + message.text
+    else:
+        text = message.text
+    bot.reply_to(message, text)
+
+
+print('bot started')
+bot.polling()
